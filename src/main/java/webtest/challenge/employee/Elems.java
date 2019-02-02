7379c208404aafb90bbba1dce6abf865c0adb8f7
chipgoss <46722108+chipgoss@users.noreply.github.com>
2019-02-01 19:21:10 -0500

    webtest commit

---
+++ src/main/java/webtest/challenge/employee/Elems.java
@@ -0,0 +1,28 @@
+package webtest.challenge.employee;
+
+//Project Web Elements
+public enum Elems {
+	LOGIN_USERNAME("form-username"),
+	LOGIN_PASSWORD("form-password"),
+	LOGIN_BUTTON("btnLogin"),
+	ADD_EMPLOYEE_BTN("btnAddEmployee"),
+	ADD_EMPLOYEE_FIRST("//*[@id=\"employees-form\"]/div[1]/div/input"),
+	ADD_EMPLOYEE_LAST("//*[@id=\"employees-form\"]/div[2]/div/input"),
+	ADD_EMPLOYEE_DEPENDENTS("//*[@id=\"employees-form\"]/div[3]/div/input"),
+	SUBMIT_BTN("//*[@id=\"employees-form\"]/div[4]/div/button[1]"),
+	FIRST_NAME("//*[@id=\"employee-table\"]/tbody/tr[2]/td[2]"),
+	LAST_NAME("//*[@id=\"employee-table\"]/tbody/tr[2]/td[3]"),
+	BENEFIT_COST("//*[@id=\"employee-table\"]/tbody/tr[2]/td[7]"),
+	NET_PAY("//*[@id=\"employee-table\"]/tbody/tr[2]/td[8]");
+
+	private final String elem;
+
+	private Elems(String elemName){	
+		elem = elemName;
+	}
+
+	public String getElem() {
+		return elem;
+	}
+	
+}
