8902fc71f443ffe2eb15ac7fec19d3d23144b417
chipgoss <46722108+chipgoss@users.noreply.github.com>
2026-07-16 14:35:57 -0400

    Updated Maven dependencis in POM and fixed Submit Elem.

--- src/main/java/webtest/challenge/demo/Elems.java
+++ src/main/java/webtest/challenge/demo/Elems.java
@@ -3,7 +3,8 @@ package webtest.challenge.demo;
 //Project Web Elements
 public enum Elems {
 	TEXT_AREA("searchbox_input"),
-	SEARCH_BTN("//*[@id=\"searchbox_homepage\"]/div/div[3]/button/span[2]");
+	SEARCH_BTN("//button[@type='submit']");
+
 
 	private final String elem;
 
